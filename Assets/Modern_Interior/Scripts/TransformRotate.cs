aa7c9528744995cd0f8a5ae5986e75e05a215ef3
Jong-min Park <ss00004@naver.com>
2023-12-10 13:08:48 +0900

    Initial commit

---
+++ Assets/Modern_Interior/Scripts/TransformRotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformRotate : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 6f, 0f) * Time.deltaTime);
+    }
+}
